3b3b28283714ec207cd9db7ea09a0ab944b309e2
Fran Leston <fran@Frans-MacBook-Air.local>
2019-02-16 16:58:37 +0100

    chore: canche db conection at seeds

--- bin/seed.js
+++ bin/seed.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 const Article = require('../models/article.model');
 
-const DB_NAME = 'rastronline'
-const MONGODB_URI = `mongodb://localhost:27017/${DB_NAME}`;
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-  .then(() => console.info(`Connected to the database: ${MONGODB_URI}`))
-  .catch(error => console.error('Database connection error:', error));
+require('../configs/db.config');
 
 const articles = [
   {
