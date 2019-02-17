041f152d5d6357bd5a7458f42771286eaa72b56a
Rodrigo Otero <roterom82@gmail.com>
2019-02-17 08:37:23 +0100

    fix: view edit diferent to 'guest' and 'admin'

--- helpers/guest.helpers.js
+++ helpers/guest.helpers.js
@@ -2,7 +2,10 @@ const constants = require('../constants');
 
 module.exports = (hbs) => {
   hbs.registerHelper("isGuest", (user, options) => {
+
+    console.log("DENTRO DEL GUEST; SESSION ES =>", user)
     if (user.role === constants.ROLES.ROLE_GUEST) {
+      console.log("es guesttt")
       return options.fn(this);
     } else {
       return options.inverse(this);
